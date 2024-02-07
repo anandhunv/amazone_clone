3fbf66709c8ed544b5dd9e1f4992d0cf23b1e399
nvxanxdhu <anandhuvasudevannv18@gmail.com>
2024-02-07 22:29:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const images=document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+
+let n = 0;
+
+function changeSlide(){
+    for(let i=0;i<images.length;i++){
+        images[i].style.display='none';
+    }
+    images[n].style.display='block';
+}
+changeSlide();
+
+next_btn.addEventListener('click',(e)=>{
+
+    if(n > 0){
+        n--;
+    }else{
+        n = images.length - 1;  
+    }
+     changeSlide();
+})
+
+prev_btn.addEventListener('click',(e)=>{
+    if(n < 0){
+        n++;
+    }else{
+        n=0;  
+    }
+     changeSlide();
+})
+
+
+
+const scrollContainer= document.querySelectorAll('.products');
+for(const item of scrollContainer){
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    })
+}
